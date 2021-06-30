a42ee4de59e699e0558dabfcfc667096b6fb2255
DESKTOP-PU250QC\FelixPC <wi20b031@technikum-wien.at>
2021-06-30 14:37:19 +0200

    Rest-Service provisorisch gestartet

---
+++ src/main/java/at/engelmaier/swlcm/randomnnumber/controllers/RandomNumberController.java
@@ -0,0 +1,17 @@
+package at.engelmaier.swlcm.randomnnumber.controllers;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.Random;
+
+/*@Path("/api/rot13/")
+public class RandomNumberController {
+    Random rand = new Random(); //instance of random class
+    int upperbound = 100;
+    //generate random values from 0-24
+    @Produces(MediaType.TEXT_PLAIN)
+    public int int_random = rand.nextInt(upperbound);
+
+
+
+}*/
